5d487340da79bf6c79fcc93499e66e9e2109c296
JadenNgoOT <90271161+JadenNgoOT@users.noreply.github.com>
2023-02-01 10:39:16 -0500

    Update Q2_PartB.py

--- Assignment 2/Q2_PartB.py
+++ Assignment 2/Q2_PartB.py
@@ -1,31 +1,29 @@
 import numpy as np
 
-def EulerChebyshev(f, df, ddf, x0, kMax, eps_x, eps_f):
-    # Input: initial point, max number of iterations, tolerance for error and residual
-
+def EulerChebyshev(f, df, ddf, x0, k_max, eps_x, eps_f):
     x = x0
-    conv = False  # flag for convergence
+    conv = False
 
     for k in range(kMax):
-        fx = f(x)  # current function value
+        fx = f(x)
         df_x = df(x)
         ddf_x = ddf(x)
-        x_new = x - (2 * fx * df_x) / (2 * (df_x ** 2) - fx * ddf_x)# update step
-        err = abs(x_new - x)  # current error estimate
-        res = abs(fx)  # current residual
+        x_new = x - (2 * fx * df_x) / (2 * (df_x ** 2) - fx * ddf_x)
+        max_err = abs(x_new - x)
+        res = abs(fx) 
 
-        print(f'Iteration {k + 1}: err={err:.4e}, res={res:.4e}')
+        print(f'Iteration {k + 1}: err={max_err:.4e}, res={res:.4e}')
 
-        if err < eps_x and res < eps_f:  # If converged ...
-            print("Converged!")
+        if max_err < eps_x and res < eps_f:
             conv = True
             break
+            
         x = x_new
 
     if (conv == False):
-        print("No convergence!")
+        print(f'No convergence after {k_max} interations')
 
-    return x, err, res, conv
+    return x, max_err, res, conv
 
 def f(x):
     return np.exp(-x) + np.cos(x + 1) - 1
@@ -36,15 +34,12 @@ def df(x):
 def ddf(x):
     return np.exp(-x) - np.cos(x + 1)
 
+x0 = 1.0  
+k_max = 100  
+eps_x = 10e-14 
+eps_f = 1e-8  
 
-#  assign parameter values
-x0 = 1.0  # initial guess
-kMax = 100  # max number of iterations
-eps_x = 10e-14  # tolerance for error
-eps_f = 1e-8  # tolerance for residual
+xstar, err, res, conv = EulerChebyshev(f, df, ddf, x0, k_max, eps_x, eps_f)
 
-#  call NewtonRaphson
-x, err, res, conv = EulerChebyshev(f, df, ddf, x0, kMax, eps_x, eps_f)
-
-#  print out solution
-print("x= " + str(x))
+if (conv):
+    print(f'\n x* = {xstar}')
