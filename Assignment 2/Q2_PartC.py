cc184e093d22941e8e36ba46ad41e519ce3806d3
JadenNgoOT <90271161+JadenNgoOT@users.noreply.github.com>
2023-02-01 10:47:21 -0500

    Update Q2_PartC.py

--- Assignment 2/Q2_PartC.py
+++ Assignment 2/Q2_PartC.py
@@ -1,61 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def EulerChebyshev(f, df, ddf, x0, kMax, eps_x, eps_f):
-    # Input: initial point, max number of iterations, tolerance for error and residual
-
+def EulerChebyshev(f, df, ddf, x0, k_max, eps_x, eps_f):
     x = x0
-    conv = False  # flag for convergence
-    errs = []
+    conv = False  
+    err_arr = []
 
-    for k in range(kMax):
-        fx = f(x)  # current function value
+    for k in range(k_max):
+        fx = f(x)  
         df_x = df(x)
         ddf_x = ddf(x)
-        x_new = x - (2 * fx * df_x) / (2 * (df_x ** 2) - fx * ddf_x)# update step
-        err = abs(x_new - x)  # current error estimate
-        res = abs(fx)  # current residual
-        errs.append(err)
+        x_new = x - (2 * fx * df_x) / (2 * (df_x ** 2) - fx * ddf_x)
+        max_err = abs(x_new - x)  
+        res = abs(fx)
+        err_arr.append(err)
 
-        print(f'Iteration {k + 1}: err={err:.4e}, res={res:.4e}')
+        print(f'Iteration {k + 1}: err={max_err:.4e}, res={res:.4e}')
 
-        if err < eps_x and res < eps_f:  # If converged ...
-            print("Converged!")
+        if max_err < eps_x and res < eps_f: 
             conv = True
             break
+            
         x = x_new
 
     if (conv == False):
-        print("No convergence!")
+       print(f'No convergence after {k_max} interations')
 
-    return x, errs, res, conv
+    return x, err_arr, res, conv
 
-def NewtonRaphson(f, df, x0, kMax, eps_x, eps_f):
+def NewtonRaphson(f, df, x0, k_max, eps_x, eps_f):
     # Input: initial point, max number of iterations, tolerance for error and residual
 
     x = x0
     conv = False  # flag for convergence
-    errs = []
+    err_arr = []
 
-    for k in range(kMax):
+    for k in range(k_max):
         fx = f(x)  # current function value
         dx = -fx / df(x)  # update step
-        err = abs(dx)  # current error estimate
+        max_err = abs(dx)  # current error estimate
         res = abs(fx)  # current residual
-        errs.append(err)
+        err_arr.append(max_err)
 
-        print(f'Iteration {k + 1}: err={err:.4e}, res={res:.4e}')
+        print(f'Iteration {k + 1}: err={max_err:.4e}, res={res:.4e}')
 
-        if err < eps_x and res < eps_f:  # If converged ...
-            print("Converged!")
+        if max_err < eps_x and res < eps_f:
             conv = True
             break
+            
         x = x + dx
 
     if (conv == False):
-        print("No convergence!")
+        print(f'No convergence after {k_max} interations')
 
-    return x, errs, res, conv
+    return x, err_arr, res, conv
 
 def f(x):
     return np.exp(-x) + np.cos(x + 1) - 1
@@ -66,18 +64,14 @@ def df(x):
 def ddf(x):
     return np.exp(-x) - np.cos(x + 1)
 
+x0 = 1.0  
+k_max = 100  
+eps_x = 10e-14  
+eps_f = 1e-8  
 
-#  assign parameter values
-x0 = 1.0  # initial guess
-k_max = 100  # max number of iterations
-eps_x = 10e-14  # tolerance for error
-eps_f = 1e-8  # tolerance for residual
-
-#  print out solution
-print("x= " + str(x))
 
 x_nr, err_nr, res_nr, conv_nr = NewtonRaphson(f, df, x0, k_max, eps_x, eps_f)
-x, err_ec, res_ec, conv = EulerChebyshev(f, df, ddf, x0, k_max, eps_x, eps_f)
+x_ec, err_ec, res_ec, conv = EulerChebyshev(f, df, ddf, x0, k_max, eps_x, eps_f)
 
 plt.semilogy(np.arange(len(err_nr)), err_nr, label='Newton-Raphson')
 plt.semilogy(np.arange(len(err_ec)), err_ec, label='EulerChebyshev')
